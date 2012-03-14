482b6219c8043db4ee0f3aea286cebe2f95d2073
Introkun <introkun@gmail.com>
2012-03-14 22:08:37 +0300

    * code cleanup (tabs replaced with spaces)

--- soapclient.js
+++ soapclient.js
@@ -9,19 +9,19 @@
 
 function SOAPClientParameters()
 {
-	var _pl = new Array();
-	this.add = function(name, value) 
-	{
-		_pl[name] = value; 
-		return this; 
-	}
-	this.toXml = function()
-	{
-		var xml = "";
-		for(var p in _pl)
-		{
-			switch(typeof(_pl[p])) 
-			{
+    var _pl = new Array();
+    this.add = function(name, value) 
+    {
+        _pl[name] = value; 
+        return this; 
+    }
+    this.toXml = function()
+    {
+        var xml = "";
+        for(var p in _pl)
+        {
+            switch(typeof(_pl[p])) 
+            {
                 case "string":
                 case "number":
                 case "boolean":
@@ -31,9 +31,9 @@ function SOAPClientParameters()
                 default:
                     break;
             }
-		}
-		return xml;	
-	}
+        }
+        return xml;	
+    }
 }
 SOAPClientParameters._serialize = function(o)
 {
@@ -115,10 +115,10 @@ SOAPClient.password = null;
 
 SOAPClient.invoke = function(url, method, parameters, async, callback)
 {
-	if(async)
-		SOAPClient._loadWsdl(url, method, parameters, async, callback);
-	else
-		return SOAPClient._loadWsdl(url, method, parameters, async, callback);
+    if(async)
+        SOAPClient._loadWsdl(url, method, parameters, async, callback);
+    else
+        return SOAPClient._loadWsdl(url, method, parameters, async, callback);
 }
 
 // private: wsdl cache
@@ -127,93 +127,93 @@ SOAPClient_cacheWsdl = new Array();
 // private: invoke async
 SOAPClient._loadWsdl = function(url, method, parameters, async, callback)
 {
-	// load from cache?
-	var wsdl = SOAPClient_cacheWsdl[url];
-	if(wsdl + "" != "" && wsdl + "" != "undefined")
-		return SOAPClient._sendSoapRequest(url, method, parameters, async, callback, wsdl);
-	// get wsdl
-	var xmlHttp = SOAPClient._getXmlHttp();
-	xmlHttp.open("GET", url + "?wsdl", async);
-	if(async) 
-	{
-		xmlHttp.onreadystatechange = function() 
-		{
-			if(xmlHttp.readyState == 4)
-				SOAPClient._onLoadWsdl(url, method, parameters, async, callback, xmlHttp);
-		}
-	}
-	xmlHttp.send(null);
-	if (!async)
-		return SOAPClient._onLoadWsdl(url, method, parameters, async, callback, xmlHttp);
+    // load from cache?
+    var wsdl = SOAPClient_cacheWsdl[url];
+    if(wsdl + "" != "" && wsdl + "" != "undefined")
+        return SOAPClient._sendSoapRequest(url, method, parameters, async, callback, wsdl);
+    // get wsdl
+    var xmlHttp = SOAPClient._getXmlHttp();
+    xmlHttp.open("GET", url + "?wsdl", async);
+    if(async) 
+    {
+        xmlHttp.onreadystatechange = function() 
+        {
+            if(xmlHttp.readyState == 4)
+                SOAPClient._onLoadWsdl(url, method, parameters, async, callback, xmlHttp);
+        }
+    }
+    xmlHttp.send(null);
+    if (!async)
+        return SOAPClient._onLoadWsdl(url, method, parameters, async, callback, xmlHttp);
 }
 SOAPClient._onLoadWsdl = function(url, method, parameters, async, callback, req)
 {
-	var wsdl = req.responseXML;
-	SOAPClient_cacheWsdl[url] = wsdl;	// save a copy in cache
-	return SOAPClient._sendSoapRequest(url, method, parameters, async, callback, wsdl);
+    var wsdl = req.responseXML;
+    SOAPClient_cacheWsdl[url] = wsdl;	// save a copy in cache
+    return SOAPClient._sendSoapRequest(url, method, parameters, async, callback, wsdl);
 }
 SOAPClient._sendSoapRequest = function(url, method, parameters, async, callback, wsdl)
 {
-	// get namespace
-	var ns = (wsdl.documentElement.attributes["targetNamespace"] + "" == "undefined") ? wsdl.documentElement.attributes.getNamedItem("targetNamespace").nodeValue : wsdl.documentElement.attributes["targetNamespace"].value;
-	// build SOAP request
-	var sr = 
-				"<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-				"<soap:Envelope " +
-				"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
-				"xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
-				"xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
-				"<soap:Body>" +
-				"<" + method + " xmlns=\"" + ns + "\">" +
-				parameters.toXml() +
-				"</" + method + "></soap:Body></soap:Envelope>";
-	// send request
-	var xmlHttp = SOAPClient._getXmlHttp();
-	if (SOAPClient.userName && SOAPClient.password){
-		xmlHttp.open("POST", url, async, SOAPClient.userName, SOAPClient.password);
-		// Some WS implementations (i.e. BEA WebLogic Server 10.0 JAX-WS) don't support Challenge/Response HTTP BASIC, so we send authorization headers in the first request
-		xmlHttp.setRequestHeader("Authorization", "Basic " + SOAPClient._toBase64(SOAPClient.userName + ":" + SOAPClient.password));
-	}
-	else
-		xmlHttp.open("POST", url, async);
-	var soapaction = ((ns.lastIndexOf("/") != ns.length - 1) ? ns + "/" : ns) + method;
-	xmlHttp.setRequestHeader("SOAPAction", soapaction);
-	xmlHttp.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
-	if(async) 
-	{
-		xmlHttp.onreadystatechange = function() 
-		{
-			if(xmlHttp.readyState == 4)
-				SOAPClient._onSendSoapRequest(method, async, callback, wsdl, xmlHttp);
-		}
-	}
-	xmlHttp.send(sr);
-	if (!async)
-		return SOAPClient._onSendSoapRequest(method, async, callback, wsdl, xmlHttp);
+    // get namespace
+    var ns = (wsdl.documentElement.attributes["targetNamespace"] + "" == "undefined") ? wsdl.documentElement.attributes.getNamedItem("targetNamespace").nodeValue : wsdl.documentElement.attributes["targetNamespace"].value;
+    // build SOAP request
+    var sr = 
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                "<soap:Envelope " +
+                "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+                "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
+                "xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                "<soap:Body>" +
+                "<" + method + " xmlns=\"" + ns + "\">" +
+                parameters.toXml() +
+                "</" + method + "></soap:Body></soap:Envelope>";
+    // send request
+    var xmlHttp = SOAPClient._getXmlHttp();
+    if (SOAPClient.userName && SOAPClient.password){
+        xmlHttp.open("POST", url, async, SOAPClient.userName, SOAPClient.password);
+        // Some WS implementations (i.e. BEA WebLogic Server 10.0 JAX-WS) don't support Challenge/Response HTTP BASIC, so we send authorization headers in the first request
+        xmlHttp.setRequestHeader("Authorization", "Basic " + SOAPClient._toBase64(SOAPClient.userName + ":" + SOAPClient.password));
+    }
+    else
+        xmlHttp.open("POST", url, async);
+    var soapaction = ((ns.lastIndexOf("/") != ns.length - 1) ? ns + "/" : ns) + method;
+    xmlHttp.setRequestHeader("SOAPAction", soapaction);
+    xmlHttp.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
+    if(async) 
+    {
+        xmlHttp.onreadystatechange = function() 
+        {
+            if(xmlHttp.readyState == 4)
+                SOAPClient._onSendSoapRequest(method, async, callback, wsdl, xmlHttp);
+        }
+    }
+    xmlHttp.send(sr);
+    if (!async)
+        return SOAPClient._onSendSoapRequest(method, async, callback, wsdl, xmlHttp);
 }
 
 SOAPClient._onSendSoapRequest = function(method, async, callback, wsdl, req) 
 {
-	var o = null;
-	var nd = SOAPClient._getElementsByTagName(req.responseXML, method + "Result");
-	if(nd.length == 0)
-		nd = SOAPClient._getElementsByTagName(req.responseXML, "return");	// PHP web Service?
-	if(nd.length == 0)
-	{
-		if(req.responseXML.getElementsByTagName("faultcode").length > 0)
-		{
-		    if(async || callback)
-		        o = new Error(500, req.responseXML.getElementsByTagName("faultstring")[0].childNodes[0].nodeValue);
-			else
-			    throw new Error(500, req.responseXML.getElementsByTagName("faultstring")[0].childNodes[0].nodeValue);			
-		}
-	}
-	else
-		o = SOAPClient._soapresult2object(nd[0], wsdl);
-	if(callback)
-		callback(o, req.responseXML);
-	if(!async)
-		return o;
+    var o = null;
+    var nd = SOAPClient._getElementsByTagName(req.responseXML, method + "Result");
+    if(nd.length == 0)
+        nd = SOAPClient._getElementsByTagName(req.responseXML, "return");	// PHP web Service?
+    if(nd.length == 0)
+    {
+        if(req.responseXML.getElementsByTagName("faultcode").length > 0)
+        {
+            if(async || callback)
+                o = new Error(500, req.responseXML.getElementsByTagName("faultstring")[0].childNodes[0].nodeValue);
+            else
+                throw new Error(500, req.responseXML.getElementsByTagName("faultstring")[0].childNodes[0].nodeValue);			
+        }
+    }
+    else
+        o = SOAPClient._soapresult2object(nd[0], wsdl);
+    if(callback)
+        callback(o, req.responseXML);
+    if(!async)
+        return o;
 }
 SOAPClient._soapresult2object = function(node, wsdl)
 {
@@ -222,96 +222,96 @@ SOAPClient._soapresult2object = function(node, wsdl)
 }
 SOAPClient._node2object = function(node, wsdlTypes)
 {
-	// null node
-	if(node == null)
-		return null;
-	// text node
-	if(node.nodeType == 3 || node.nodeType == 4)
-		return SOAPClient._extractValue(node, wsdlTypes);
-	// leaf node
-	if (node.childNodes.length == 1 && (node.childNodes[0].nodeType == 3 || node.childNodes[0].nodeType == 4))
-		return SOAPClient._node2object(node.childNodes[0], wsdlTypes);
-	var isarray = SOAPClient._getTypeFromWsdl(node.nodeName, wsdlTypes).toLowerCase().indexOf("arrayof") != -1;
-	// object node
-	if(!isarray)
-	{
-		var obj = null;
-		if(node.hasChildNodes())
-			obj = new Object();
-		for(var i = 0; i < node.childNodes.length; i++)
-		{
-			var p = SOAPClient._node2object(node.childNodes[i], wsdlTypes);
-			obj[node.childNodes[i].nodeName] = p;
-		}
-		return obj;
-	}
-	// list node
-	else
-	{
-		// create node ref
-		var l = new Array();
-		for(var i = 0; i < node.childNodes.length; i++)
-			l[l.length] = SOAPClient._node2object(node.childNodes[i], wsdlTypes);
-		return l;
-	}
-	return null;
+    // null node
+    if(node == null)
+        return null;
+    // text node
+    if(node.nodeType == 3 || node.nodeType == 4)
+        return SOAPClient._extractValue(node, wsdlTypes);
+    // leaf node
+    if (node.childNodes.length == 1 && (node.childNodes[0].nodeType == 3 || node.childNodes[0].nodeType == 4))
+        return SOAPClient._node2object(node.childNodes[0], wsdlTypes);
+    var isarray = SOAPClient._getTypeFromWsdl(node.nodeName, wsdlTypes).toLowerCase().indexOf("arrayof") != -1;
+    // object node
+    if(!isarray)
+    {
+        var obj = null;
+        if(node.hasChildNodes())
+            obj = new Object();
+        for(var i = 0; i < node.childNodes.length; i++)
+        {
+            var p = SOAPClient._node2object(node.childNodes[i], wsdlTypes);
+            obj[node.childNodes[i].nodeName] = p;
+        }
+        return obj;
+    }
+    // list node
+    else
+    {
+        // create node ref
+        var l = new Array();
+        for(var i = 0; i < node.childNodes.length; i++)
+            l[l.length] = SOAPClient._node2object(node.childNodes[i], wsdlTypes);
+        return l;
+    }
+    return null;
 }
 SOAPClient._extractValue = function(node, wsdlTypes)
 {
-	var value = node.nodeValue;
-	switch(SOAPClient._getTypeFromWsdl(node.parentNode.nodeName, wsdlTypes).toLowerCase())
-	{
-		default:
-		case "s:string":			
-			return (value != null) ? value + "" : "";
-		case "s:boolean":
-			return value + "" == "true";
-		case "s:int":
-		case "s:long":
-			return (value != null) ? parseInt(value + "", 10) : 0;
-		case "s:double":
-			return (value != null) ? parseFloat(value + "") : 0;
-		case "s:datetime":
-			if(value == null)
-				return null;
-			else
-			{
-				value = value + "";
-				value = value.substring(0, (value.lastIndexOf(".") == -1 ? value.length : value.lastIndexOf(".")));
-				value = value.replace(/T/gi," ");
-				value = value.replace(/-/gi,"/");
-				var d = new Date();
-				d.setTime(Date.parse(value));										
-				return d;				
-			}
-	}
+    var value = node.nodeValue;
+    switch(SOAPClient._getTypeFromWsdl(node.parentNode.nodeName, wsdlTypes).toLowerCase())
+    {
+        default:
+        case "s:string":			
+            return (value != null) ? value + "" : "";
+        case "s:boolean":
+            return value + "" == "true";
+        case "s:int":
+        case "s:long":
+            return (value != null) ? parseInt(value + "", 10) : 0;
+        case "s:double":
+            return (value != null) ? parseFloat(value + "") : 0;
+        case "s:datetime":
+            if(value == null)
+                return null;
+            else
+            {
+                value = value + "";
+                value = value.substring(0, (value.lastIndexOf(".") == -1 ? value.length : value.lastIndexOf(".")));
+                value = value.replace(/T/gi," ");
+                value = value.replace(/-/gi,"/");
+                var d = new Date();
+                d.setTime(Date.parse(value));										
+                return d;				
+            }
+    }
 }
 SOAPClient._getTypesFromWsdl = function(wsdl)
 {
-	var wsdlTypes = new Array();
-	// IE
-	var ell = wsdl.getElementsByTagName("s:element");	
-	var useNamedItem = true;
-	// MOZ
-	if(ell.length == 0)
-	{
-		ell = wsdl.getElementsByTagName("element");	     
-		useNamedItem = false;
-	}
-	for(var i = 0; i < ell.length; i++)
-	{
-		if(useNamedItem)
-		{
-			if(ell[i].attributes.getNamedItem("name") != null && ell[i].attributes.getNamedItem("type") != null) 
-				wsdlTypes[ell[i].attributes.getNamedItem("name").nodeValue] = ell[i].attributes.getNamedItem("type").nodeValue;
-		}	
-		else
-		{
-			if(ell[i].attributes["name"] != null && ell[i].attributes["type"] != null)
-				wsdlTypes[ell[i].attributes["name"].value] = ell[i].attributes["type"].value;
-		}
-	}
-	return wsdlTypes;
+    var wsdlTypes = new Array();
+    // IE
+    var ell = wsdl.getElementsByTagName("s:element");	
+    var useNamedItem = true;
+    // MOZ
+    if(ell.length == 0)
+    {
+        ell = wsdl.getElementsByTagName("element");	     
+        useNamedItem = false;
+    }
+    for(var i = 0; i < ell.length; i++)
+    {
+        if(useNamedItem)
+        {
+            if(ell[i].attributes.getNamedItem("name") != null && ell[i].attributes.getNamedItem("type") != null) 
+                wsdlTypes[ell[i].attributes.getNamedItem("name").nodeValue] = ell[i].attributes.getNamedItem("type").nodeValue;
+        }	
+        else
+        {
+            if(ell[i].attributes["name"] != null && ell[i].attributes["type"] != null)
+                wsdlTypes[ell[i].attributes["name"].value] = ell[i].attributes["type"].value;
+        }
+    }
+    return wsdlTypes;
 }
 SOAPClient._getTypeFromWsdl = function(elementname, wsdlTypes)
 {
@@ -321,89 +321,89 @@ SOAPClient._getTypeFromWsdl = function(elementname, wsdlTypes)
 // private: utils
 SOAPClient._getElementsByTagName = function(document, tagName)
 {
-	try
-	{
-		// trying to get node omitting any namespaces (latest versions of MSXML.XMLDocument)
-		return document.selectNodes(".//*[local-name()=\""+ tagName +"\"]");
-	}
-	catch (ex) {}
-	// old XML parser support
-	return document.getElementsByTagName(tagName);
+    try
+    {
+        // trying to get node omitting any namespaces (latest versions of MSXML.XMLDocument)
+        return document.selectNodes(".//*[local-name()=\""+ tagName +"\"]");
+    }
+    catch (ex) {}
+    // old XML parser support
+    return document.getElementsByTagName(tagName);
 }
 // private: xmlhttp factory
 SOAPClient._getXmlHttp = function() 
 {
-	try
-	{
-		if(window.XMLHttpRequest) 
-		{
-			var req = new XMLHttpRequest();
-			// some versions of Moz do not support the readyState property and the onreadystate event so we patch it!
-			if(req.readyState == null) 
-			{
-				req.readyState = 1;
-				req.addEventListener("load", 
-									function() 
-									{
-										req.readyState = 4;
-										if(typeof req.onreadystatechange == "function")
-											req.onreadystatechange();
-									},
-									false);
-			}
-			return req;
-		}
-		if(window.ActiveXObject) 
-			return new ActiveXObject(SOAPClient._getXmlHttpProgID());
-	}
-	catch (ex) {}
-	throw new Error("Your browser does not support XmlHttp objects");
+    try
+    {
+        if(window.XMLHttpRequest) 
+        {
+            var req = new XMLHttpRequest();
+            // some versions of Moz do not support the readyState property and the onreadystate event so we patch it!
+            if(req.readyState == null) 
+            {
+                req.readyState = 1;
+                req.addEventListener("load", 
+                                    function() 
+                                    {
+                                        req.readyState = 4;
+                                        if(typeof req.onreadystatechange == "function")
+                                            req.onreadystatechange();
+                                    },
+                                    false);
+            }
+            return req;
+        }
+        if(window.ActiveXObject) 
+            return new ActiveXObject(SOAPClient._getXmlHttpProgID());
+    }
+    catch (ex) {}
+    throw new Error("Your browser does not support XmlHttp objects");
 }
 SOAPClient._getXmlHttpProgID = function()
 {
-	if(SOAPClient._getXmlHttpProgID.progid)
-		return SOAPClient._getXmlHttpProgID.progid;
-	var progids = ["Msxml2.XMLHTTP.5.0", "Msxml2.XMLHTTP.4.0", "MSXML2.XMLHTTP.3.0", "MSXML2.XMLHTTP", "Microsoft.XMLHTTP"];
-	var o;
-	for(var i = 0; i < progids.length; i++)
-	{
-		try
-		{
-			o = new ActiveXObject(progids[i]);
-			return SOAPClient._getXmlHttpProgID.progid = progids[i];
-		}
-		catch (ex) {};
-	}
-	throw new Error("Could not find an installed XML parser");
+    if(SOAPClient._getXmlHttpProgID.progid)
+        return SOAPClient._getXmlHttpProgID.progid;
+    var progids = ["Msxml2.XMLHTTP.5.0", "Msxml2.XMLHTTP.4.0", "MSXML2.XMLHTTP.3.0", "MSXML2.XMLHTTP", "Microsoft.XMLHTTP"];
+    var o;
+    for(var i = 0; i < progids.length; i++)
+    {
+        try
+        {
+            o = new ActiveXObject(progids[i]);
+            return SOAPClient._getXmlHttpProgID.progid = progids[i];
+        }
+        catch (ex) {};
+    }
+    throw new Error("Could not find an installed XML parser");
 }
 
 SOAPClient._toBase64 = function(input)
 {
-	var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-	var output = "";
-	var chr1, chr2, chr3;
-	var enc1, enc2, enc3, enc4;
-	var i = 0;
+    var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var output = "";
+    var chr1, chr2, chr3;
+    var enc1, enc2, enc3, enc4;
+    var i = 0;
 
-	do {
-		chr1 = input.charCodeAt(i++);
-		chr2 = input.charCodeAt(i++);
-		chr3 = input.charCodeAt(i++);
+    do {
+        chr1 = input.charCodeAt(i++);
+        chr2 = input.charCodeAt(i++);
+        chr3 = input.charCodeAt(i++);
 
-		enc1 = chr1 >> 2;
-		enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-		enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-		enc4 = chr3 & 63;
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
 
-		if (isNaN(chr2)) {
-			enc3 = enc4 = 64;
-		} else if (isNaN(chr3)) {
-			enc4 = 64;
-		}
+        if (isNaN(chr2)) {
+            enc3 = enc4 = 64;
+        } else if (isNaN(chr3)) {
+            enc4 = 64;
+        }
 
-		output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) +
-		keyStr.charAt(enc3) + keyStr.charAt(enc4);
-	} while (i < input.length);
+        output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) +
+        keyStr.charAt(enc3) + keyStr.charAt(enc4);
+    } while (i < input.length);
 
-	return output;
+    return output;
 }
